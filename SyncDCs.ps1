75084d7c5093403373640915e5060ec681a55b79
Peter Faxe Lachenmair <peterlachenmair.info>
2019-06-24 15:11:38 +0200

    syncdc.ps1 added
    syncdc.ps1 added

---
+++ SyncDCs.ps1
@@ -0,0 +1,14 @@
+﻿#SyncDCs.ps1
+#
+# Synopsis
+# 
+# Force AD Replication for all existing DCs
+#
+#
+################################################################
+$DomainControllers = Get-ADDomainController -Filter *
+ForEach ($DC in $DomainControllers.Name) {
+    Write-Host "Processing for "$DC -ForegroundColor Green
+    REPADMIN /kcc $DC
+    REPADMIN /syncall /A /e /q $DC
+}
